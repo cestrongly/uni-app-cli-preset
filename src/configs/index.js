bfe9486dcc01e08fe639246b7b8fdd3345a9dbc9
wangProgrammer <itwangyang@foxmail.com>
2022-05-26 15:47:48 +0800

    feat:搭建了架构基于VueCli模式的uniapp脚手架集成 uni-app+ vue-cli+ uview+ vue-rouer+ vuex+ axios+ request+ tailwindcss uview-ui2.x z-paging 底层架构

---
+++ src/configs/index.js
@@ -0,0 +1,13 @@
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+module.exports = {
+  title: 'vue-cli-wangyang',
+  logo: () => require('@/static/images/logo.png'),
+  showMocks: () => isDevelopment,
+  // 项目基础路径
+  appBasePath: isProduction ? '' : '',
+  // CDN 资源基础域名
+  assetURL: '',
+  // 请求 基础域名
+  requsetURL: 'http://cims_api.chetell.com/api/v1',
+};
