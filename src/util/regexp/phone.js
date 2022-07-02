a06fb2d615ed0b7042cd9d023ce2e93c6d963db5
不会淹死的鱼 <cestrmail@163.com>
2022-07-02 14:39:20 +0800

    更新： - 工具包   - 创建正则表达式目录

---
+++ src/util/regexp/phone.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: cest
+ * @Date: 2022-06-30 14:32:22
+ * @LastEditTime: 2022-06-30 14:34:14
+ * @LastEditors: cest
+ * @FilePath: /cedo-app-cli/src/util/regexp/phone.js
+ * @Description: 正则表达式 手机号码
+ */
+
+export const phone = /^1((3[\d])|(4[5,6,9])|(5[0-3,5-9])|(6[5-7])|(7[0-8])|(8[1-8])|(9[1,8,9]))\d{8}$/;
