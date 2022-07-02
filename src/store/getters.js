a66d6684f04db2bfa45bca793818372cc14d7e7c
不会淹死的鱼 <cestrmail@163.com>
2022-07-02 14:03:54 +0800

    store 配置

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,31 @@
+/**
+ * @Author: cest
+ * @Date: 2022-06-20 08:11:57
+ * @LastEditTime: 2022-06-29 19:27:05
+ * @LastEditors: cest
+ * @FilePath: /cedo-app-cli/src/store/getters.js
+ * @Description: 编辑描述内容
+ */
+import test from '@/uni_modules/uview-ui/libs/function/test'
+import * as STRING_MAP from '@/store/string_map'
+const {
+	isEmpty
+} = test
 export default {
-  token: (state) => state.user.token,
-  siteData: (state) => state.site.siteData,
-  userData: (state) => state.user.userData,
-  userId: (state) => state.user.userData.id,
-};
+	defaultValue: (state, getters) => (value) => {
+		return isEmpty(value) ? '' : value
+	},
+
+  getEnvVersion (state, getters) {
+    const accountInfo = uni.getAccountInfoSync();
+    let version = state[STRING_MAP.envVersion][accountInfo, accountInfo.miniProgram.envVersion]
+    if (version !== '正式版') {
+      return `(${version})`
+    } else {
+      return ''
+    }
+  },
+  getAppTitle(state,getters) {
+    return process.env['VUE_APP_TITLE']
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,78 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import getters from './getters';
-import user from './modules/user';
-import site from './modules/site';
-import dicts from './modules/dicts';
+/**
+ * @Author: cest
+ * @Date: 2022-06-17 23:02:14
+ * @LastEditTime: 2022-07-02 12:57:51
+ * @LastEditors: cest
+ * @FilePath: /uni-app-cli/src/store/index.js
+ * @Description: vuex 状态管理总管
+ */
 
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { createLogger } from 'vuex'
+import state from './state'
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
 
+Vue.use(Vuex)
+
+// 动态引入 store modules
+// 主要解决的问题每次建一个module需要自己去主index.js里面去注册
+// 为了偷懒，也为了避免团队开发时同时对index.js 进行修改引发冲突
+// 所以在index.js中 动态的对子目录和模块进行注册
+
+function loadModules() {
+  const context = require.context('./modules', true, /([a-z_]+)\.js$/i)
+
+  const modules = context
+    .keys()
+    .map(key => ({
+      key,
+      name: key.match(/([a-z_]+)\.js$/i)[1]
+    }))
+    .reduce(
+      (modules, { key, name }) => ({
+        ...modules,
+        [name]: context(key).default
+      }),
+      {}
+    )
+
+  return {
+    context,
+    modules
+  }
+}
+
+const { context, modules } = loadModules()
 const store = new Vuex.Store({
-  modules: {
-    user,
-    site,
-    dicts,
-  },
+  // Vuex 自带一个日志插件用于一般的调试:
+  plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
+
+  // 开启严格模式
+  // 在严格模式下，无论何时发生了状态变更且不是由 mutation 函数引起的，将会抛出错误。这能保证所有的状态变更都能被调试工具跟踪到。
+
+  // 开发环境与发布环境#
+  // 不要在发布环境下启用严格模式！严格模式会深度监测状态树来检测不合规的状态变更——请确保在发布环境下关闭严格模式，以避免性能损失。
+
+  strict: process.env.NODE_ENV !== 'production',
+  state,
   getters,
-});
+  mutations,
+  actions,
+  modules
+})
 
-// Vue.prototype.$store = store;
+if (module.hot) {
+  // 在任何模块发生改变时进行热重载。
+  module.hot.accept(context.id, () => {
+    const { modules } = loadModules()
 
-export default store;
+    store.hotUpdate({
+      modules
+    })
+  })
+}
+
+export default store
