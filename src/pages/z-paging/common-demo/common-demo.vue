851f9259c29fb62e75d04600952b76fefeeb824c
cestrmail@163.com <cestrmail@163.com>
2022-07-23 15:34:18 +0800

    fix

--- src/pages/z-paging/common-demo/common-demo.vue
+++ src/pages/z-paging/common-demo/common-demo.vue
@@ -33,32 +33,35 @@ export default {
   computed: {},
   methods: {
     tabChange(index) {
-				this.tabIndex = index;
-				// 当切换tab或搜索时请调用组件的reload方法，请勿直接调用：queryList方法！！
-				this.$refs.paging.reload();
-			},
-			queryList(pageNo, pageSize) {
-				// 组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
-				// 这里的pageNo和pageSize会自动计算好，直接传给服务器即可
-				// 模拟请求服务器获取分页数据，请替换成自己的网络请求
-				const params = {
-					pageNo,
-					pageSize,
-					type: this.tabIndex + 1
-				}
-				$request.queryList(params).then(res => {
-					// 将请求的结果数组传递给z-paging
-					this.$refs.paging.complete(res.data.list);
-				}).catch(res => {
-					// 如果请求失败写this.$refs.paging.complete(false);
-					// 注意，每次都需要在catch中写这句话很麻烦，z-paging提供了方案可以全局统一处理
-					// 在底层的网络请求抛出异常时，写uni.$emit('z-paging-error-emit');即可
-					this.$refs.paging.complete(false);
-				})
-			},
-			itemClick(item, index) {
-				console.log('点击了', item.title);
-			}
+      this.tabIndex = index
+      // 当切换tab或搜索时请调用组件的reload方法，请勿直接调用：queryList方法！！
+      this.$refs.paging.reload()
+    },
+    queryList(pageNo, pageSize) {
+      // 组件加载时会自动触发此方法，因此默认页面加载时会自动触发，无需手动调用
+      // 这里的pageNo和pageSize会自动计算好，直接传给服务器即可
+      // 模拟请求服务器获取分页数据，请替换成自己的网络请求
+      const params = {
+        pageNo,
+        pageSize,
+        type: this.tabIndex + 1
+      }
+      $request
+        .queryList(params)
+        .then(res => {
+          // 将请求的结果数组传递给z-paging
+          this.$refs.paging.complete(res.data.list)
+        })
+        .catch(res => {
+          // 如果请求失败写this.$refs.paging.complete(false);
+          // 注意，每次都需要在catch中写这句话很麻烦，z-paging提供了方案可以全局统一处理
+          // 在底层的网络请求抛出异常时，写uni.$emit('z-paging-error-emit');即可
+          this.$refs.paging.complete(false)
+        })
+    },
+    itemClick(item, index) {
+      console.log('点击了', item.title)
+    }
   },
   watch: {},
 
@@ -85,28 +88,28 @@ export default {
 
 <style scoped>
 .item {
-		position: relative;
-		height: 150rpx;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0rpx 30rpx;
-	}
+  position: relative;
+  height: 150rpx;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0rpx 30rpx;
+}
 
-	.item-detail {
-		padding: 5rpx 15rpx;
-		border-radius: 10rpx;
-		font-size: 28rpx;
-		color: white;
-		background-color: #007AFF;
-	}
+.item-detail {
+  padding: 5rpx 15rpx;
+  border-radius: 10rpx;
+  font-size: 28rpx;
+  color: white;
+  background-color: #007aff;
+}
 
-	.item-line {
-		position: absolute;
-		bottom: 0rpx;
-		left: 0rpx;
-		height: 1px;
-		width: 100%;
-		background-color: #eeeeee;
-	}
+.item-line {
+  position: absolute;
+  bottom: 0rpx;
+  left: 0rpx;
+  height: 1px;
+  width: 100%;
+  background-color: #eeeeee;
+}
 </style>
