0d6cf38d8de4a915d664747cf852600a79f98192
不会淹死的鱼 <cestrmail@163.com>
2022-07-30 09:53:39 +0800

    代码注释

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: cest
  * @Date: 2022-07-09 13:14:15
- * @LastEditTime: 2022-07-30 08:28:30
+ * @LastEditTime: 2022-07-30 09:00:27
  * @LastEditors: cest
  * @FilePath: /uni-app-cli-preset/src/App.vue
  * @Description: 编辑描述内容
@@ -54,26 +54,43 @@ export default {
       success: function (e) {
         // #ifndef MP
         // 微信小程序/支付宝小程序/百度小程序/头条小程序/QQ小程序
-        Vue.prototype.$StatusBar = e.statusBarHeight
+
+        // 手机状态栏的高度
+        Vue.prototype.$StatusBarHeight = e.statusBarHeight
+        // platform	客户端平台，值域为：ios、android、mac（3.1.10+）、windows（3.1.10+）、linux（3.1.10+）
         if (e.platform === 'android') {
-          Vue.prototype.$CustomBar = e.statusBarHeight + 50
+          // 自定义导航栏--高度
+          Vue.prototype.$CustomStatusBarHeight = e.statusBarHeight + 50
         } else {
-          Vue.prototype.$CustomBar = e.statusBarHeight + 45
+          // 自定义导航栏--高度
+          Vue.prototype.$CustomStatusBarHeight = e.statusBarHeight + 45
         }
         // #endif
 
         // #ifdef MP-WEIXIN
         // 微信小程序
-        Vue.prototype.$StatusBar = e.statusBarHeight
+
+        // 手机状态栏的高度
+        Vue.prototype.$StatusBarHeight = e.statusBarHeight
+
+        // getMenuButtonBoundingClientRect()
+        // 在小程序平台，如果原生导航栏被隐藏，仍然在右上角会有一个悬浮按钮，微信下也被称为胶囊按钮。
+        // 本API用于获取小程序下该菜单按钮的布局位置信息，方便开发者布局顶部内容时避开该按钮。
+        // 坐标信息以屏幕左上角为原点。
+
         const custom = wx.getMenuButtonBoundingClientRect()
-        Vue.prototype.$Custom = custom
-        Vue.prototype.$CustomBar = custom.bottom + custom.top - e.statusBarHeight + 4
+        Vue.prototype.$MenuButtonBoundingClientRect = custom
+        // 自定义导航栏--高度
+        Vue.prototype.$CustomStatusBarHeight = custom.bottom + custom.top - e.statusBarHeight + 4
         // #endif
 
         // #ifdef MP-ALIPAY
         // 支付宝小程序
-        Vue.prototype.$StatusBar = e.statusBarHeight
-        Vue.prototype.$CustomBar = e.statusBarHeight + e.titleBarHeight
+
+        // 手机状态栏的高度
+        Vue.prototype.$StatusBarHeight = e.statusBarHeight
+        // 自定义导航栏--高度
+        Vue.prototype.$CustomStatusBarHeight = e.statusBarHeight + e.titleBarHeight
         // #endif
       }
     })
